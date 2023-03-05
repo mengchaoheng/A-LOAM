62dbc270cf8a02a464a667e390e6e256f4b4cf00
mengchaoheng <chaohengmeng@163.com>
2023-03-05 14:43:35 +0800

    fix 3d lidar  slam

--- src/kittiHelper.cpp
+++ src/kittiHelper.cpp
@@ -50,7 +50,7 @@ int main(int argc, char** argv)
     n.getParam("publish_delay", publish_delay);
     publish_delay = publish_delay <= 0 ? 1 : publish_delay;
 
-    ros::Publisher pub_laser_cloud = n.advertise<sensor_msgs::PointCloud2>("/velodyne_points", 2);
+    ros::Publisher pub_laser_cloud = n.advertise<sensor_msgs::PointCloud2>("/iris_0/velodyne_points", 2);
 
     image_transport::ImageTransport it(n);
     image_transport::Publisher pub_image_left = it.advertise("/image_left", 2);
